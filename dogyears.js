e986b9d76e626db6963bf3717a75a67c15b76fde
Solijon Sharipov <40540002+Solijons@users.noreply.github.com>
2018-12-25 14:04:47 -0800

    Add files via upload

---
+++ dogyears.js
@@ -0,0 +1,14 @@
+
+const myAge = 22; // this is my age 
+let earlyYears = 2; // value 
+earlyYears *= 10.5;// changing value by multipling
+let laterYears = myAge -2; //equaling the age
+
+laterYears *= 4; // mutiplied the value of the myAge
+console.log(`The early years is ${earlyYears}`);
+console.log(`and the laterYears ${laterYears}.`)
+
+let myAgeInDogYears = (earlyYears, laterYears); 
+let myName = 'Solijon Sharipov'
+myName.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)// this is console
